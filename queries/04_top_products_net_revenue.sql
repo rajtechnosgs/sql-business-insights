1e5f76a0c1efe5ecd1cd70d07d2058c705a5ca01
Raj Dev <161154549+rajtechnosgs@users.noreply.github.com>
2026-07-08 12:54:16 +0530

    Update 04_top_products_net_revenue.sql

--- queries/04_top_products_net_revenue.sql
+++ queries/04_top_products_net_revenue.sql
@@ -1,1 +1,55 @@
 
+-- Q4: Top Products by Net Revenue (After Refunds)
+
+with product_revenue as (
+    select
+        p.product_id
+      , p.product_name
+      , c.category_name
+      , sum(oi.qty)                       as units_sold
+      , sum(oi.unit_price * oi.qty)        as gross_revenue
+      , count(distinct oi.order_id)        as order_count
+    from ecom.order_items oi
+    join ecom.product_variants pv on oi.variant_id = pv.variant_id
+    join ecom.products p on pv.product_id = p.product_id
+    join ecom.categories c on c.category_id = p.category_id
+    group by 1, 2, 3
+)
+
+, product_returns as (
+    select
+        p.product_id
+      , count(*) as returns_count
+    from ecom.return_items ri
+    join ecom.product_variants pv on ri.variant_id = pv.variant_id
+    join ecom.products p on pv.product_id = p.product_id
+    group by 1
+)
+
+, product_refunds as (
+    select
+        p.product_id
+      , sum(rf.amount) as refunds_amount
+    from ecom.refunds rf
+    join ecom.return_requests rr on rf.order_id = rr.order_id
+    join ecom.return_items ri on rr.return_id = ri.return_id
+    join ecom.product_variants pv on ri.variant_id = pv.variant_id
+    join ecom.products p on pv.product_id = p.product_id
+    group by 1
+)
+
+select
+    pr.product_id
+  , pr.product_name
+  , pr.category_name
+  , pr.gross_revenue
+  , pr.order_count                                                        as orders_count
+  , pr.units_sold
+  , coalesce(pret.returns_count, 0)                                       as returns_count
+  , coalesce(pret.returns_count, 0) * 1.0 / nullif(pr.order_count, 0)     as return_rate
+  , coalesce(pf.refunds_amount, 0)                                        as refunds_amount
+  , pr.gross_revenue - coalesce(pf.refunds_amount, 0)                     as net_revenue
+from product_revenue pr
+left join product_returns pret on pr.product_id = pret.product_id
+left join product_refunds pf on pr.product_id = pf.product_id
+order by net_revenue desc;
